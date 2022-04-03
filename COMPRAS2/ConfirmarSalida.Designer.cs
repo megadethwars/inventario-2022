6d22b279eacd460c252711c1fdfe4d852d05b5fe
DESKTOP-FGFDBVD\leone <leonellopezvazquez2@gmail.com>
2022-04-02 21:39:02 -0600

    init confirmar salida

--- COMPRAS2/ConfirmarSalida.Designer.cs
+++ COMPRAS2/ConfirmarSalida.Designer.cs
@@ -31,17 +31,24 @@ namespace COMPRAS2
         {
             this.lblConfirmarSalida = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.tbpass = new System.Windows.Forms.TextBox();
+            this.cbLugares = new System.Windows.Forms.ComboBox();
+            this.bl1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btAceptar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblConfirmarSalida
             // 
             this.lblConfirmarSalida.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblConfirmarSalida.AutoSize = true;
-            this.lblConfirmarSalida.Font = new System.Drawing.Font("Knockout 48 Featherweight", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmarSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblConfirmarSalida.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(202)))));
             this.lblConfirmarSalida.Location = new System.Drawing.Point(560, 30);
             this.lblConfirmarSalida.Name = "lblConfirmarSalida";
-            this.lblConfirmarSalida.Size = new System.Drawing.Size(202, 42);
+            this.lblConfirmarSalida.Size = new System.Drawing.Size(357, 39);
             this.lblConfirmarSalida.TabIndex = 70;
             this.lblConfirmarSalida.Text = "CONFIRMAR SALIDA";
             // 
@@ -60,17 +67,87 @@ namespace COMPRAS2
             this.btnBack.TabIndex = 69;
             this.btnBack.UseVisualStyleBackColor = true;
             // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Location = new System.Drawing.Point(540, 192);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(216, 20);
+            this.tbUsuario.TabIndex = 71;
+            // 
+            // tbpass
+            // 
+            this.tbpass.Location = new System.Drawing.Point(540, 286);
+            this.tbpass.Name = "tbpass";
+            this.tbpass.Size = new System.Drawing.Size(216, 20);
+            this.tbpass.TabIndex = 72;
+            // 
+            // cbLugares
+            // 
+            this.cbLugares.FormattingEnabled = true;
+            this.cbLugares.Location = new System.Drawing.Point(540, 390);
+            this.cbLugares.Name = "cbLugares";
+            this.cbLugares.Size = new System.Drawing.Size(216, 21);
+            this.cbLugares.TabIndex = 73;
+            // 
+            // bl1
+            // 
+            this.bl1.AutoSize = true;
+            this.bl1.ForeColor = System.Drawing.SystemColors.Control;
+            this.bl1.Location = new System.Drawing.Point(537, 157);
+            this.bl1.Name = "bl1";
+            this.bl1.Size = new System.Drawing.Size(43, 13);
+            this.bl1.TabIndex = 74;
+            this.bl1.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(537, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 75;
+            this.label1.Text = "Contraseña";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(537, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 76;
+            this.label2.Text = "Lugar de destino";
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Location = new System.Drawing.Point(540, 461);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(112, 38);
+            this.btAceptar.TabIndex = 77;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.btAceptar_Click);
+            // 
             // ConfirmarSalida
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1279, 861);
+            this.Controls.Add(this.btAceptar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bl1);
+            this.Controls.Add(this.cbLugares);
+            this.Controls.Add(this.tbpass);
+            this.Controls.Add(this.tbUsuario);
             this.Controls.Add(this.lblConfirmarSalida);
             this.Controls.Add(this.btnBack);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ConfirmarSalida";
             this.Text = "ConfirmarSalida";
+            this.Load += new System.EventHandler(this.ConfirmarSalida_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,5 +157,12 @@ namespace COMPRAS2
 
         private System.Windows.Forms.Label lblConfirmarSalida;
         private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.TextBox tbpass;
+        private System.Windows.Forms.ComboBox cbLugares;
+        private System.Windows.Forms.Label bl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btAceptar;
     }
 }
--- COMPRAS2/ConfirmarSalida.cs
+++ COMPRAS2/ConfirmarSalida.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using COMPRAS2.modelos;
+using COMPRAS2.servicios;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +10,169 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Converters;
 
 namespace COMPRAS2
 {
     public partial class ConfirmarSalida : Form
     {
+        List<Tuple<Int32, String>> listaLugares;
+
+        private int idlugar;
+        private int idUsuario;
+
         public ConfirmarSalida()
         {
             InitializeComponent();
+        }
+
+        private async void ConfirmarSalida_Load(object sender, EventArgs e)
+        {
+            int lugars = await Lugares();
+        }
+
+
+        private async Task<int> Lugares()
+        {
+            try
+            {
+                var url = HttpMethods.url + "lugares";
+                StatusMessage statusmessage = await HttpMethods.get(url);
+
+                if (statusmessage.statuscode == 500)
+                {
+                    MessageBox.Show("Error interno del servidor");
+                    return 2;
+                }
+
+                if (statusmessage.statuscode == 404)
+                {
+                    MessageBox.Show("Estatus no encontrados");
+                    return 2;
+                }
+
+                if (statusmessage.statuscode != 200)
+                {
+                    return 1;
+                }
+
+                var lugares = JsonConvert.DeserializeObject<List<Lugares>>(statusmessage.data);
+                listaLugares.Add(Tuple.Create<Int32, String>(0, "ninguno"));
+                for (int x = 0; x < lugares.Count; x++)
+                {
+                    listaLugares.Add(Tuple.Create<Int32, String>(lugares[x].id, lugares[x].lugar));
+                }
+                cbLugares.DataSource = listaLugares;
+                cbLugares.DisplayMember = "Item2";
+                cbLugares.ValueMember = "Item1";
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Occurrio un error en la respuesta, reintente de nuevo ");
+                return 10;
+            }
+        }
+
+
+        private async Task<int> Auth()
+        {
+            try
+            {
+                if (tbUsuario.Text == "")
+                {
+                    MessageBox.Show("campo de usuario vacio");
+                    return 1;
+                }
+
+                if (tbpass.Text == "")
+                {
+                    MessageBox.Show("campo de password vacio");
+                    return 1;
+                }
+
+                modelos.User user = new User();
+                user.username = tbUsuario.Text;
+                user.password = tbpass.Text;
+                user.id = 0;
+                user.rolId = 0;
+                user.nombre = null;
+                user.statusId = 0;
+                user.telefono = null;
+                user.correo = null;
+
+
+
+                string json = JsonConvert.SerializeObject(user,
+                new JsonSerializerSettings { DefaultValueHandling = DefaultValueHandling.Ignore });
+                var url = HttpMethods.url + "usuarios/login";
+                StatusMessage statusmessage = await HttpMethods.Post(url, json);
+
+                if (statusmessage.statuscode == 404)
+                {
+
+                    MessageBox.Show("Usuario no encontrado");
+                    return 2;
+                }
+
+                if (statusmessage.statuscode == 401)
+                {
+
+                    MessageBox.Show("Usuario y/o contraseña incorrecto");
+                    return 2;
+                }
+
+                if (statusmessage.statuscode == 500)
+                {
+
+                    MessageBox.Show("Error interno del servidor");
+                    return 2;
+                }
+
+                if (statusmessage.statuscode != 201)
+                {
+                    MessageBox.Show("Usuario y/o contraseña incorrectos");
+                    return 2;
+                }
+
+                User userDeserialize = JsonConvert.DeserializeObject<User>(statusmessage.data, new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSz" });
+                idUsuario = userDeserialize.id;
+                
+
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Occurrio un error en la respuesta, reintente de nuevo ");
+                return 10;
+            }
+
+        }
+
+        private async void btAceptar_Click(object sender, EventArgs e)
+        {
+            if (idlugar == 0) {
+                MessageBox.Show("No se ha asignado algun lugar , intente de nuevo");
+            }
+
+            int statusUser = await Auth();
+
+            if (statusUser != 0) {
+                return;
+            }
+
+            if (cbLugares.SelectedItem != null)
+            {
+                var idLugarestuple = (Tuple<int, string>)cbLugares.SelectedItem;
+                idlugar = idLugarestuple.Item1;           
+            }
+            else
+            {
+                MessageBox.Show("No se ha seleccionado ningun lugar");
+                return;
+            }
         }
     }
 }
