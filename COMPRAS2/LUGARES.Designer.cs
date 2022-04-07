91b699ce16fd8f9e1cceb391865197dc686b452f
Reachkoatl <rodrigoesau2012@gmail.com>
2022-04-07 01:07:43 -0500

    se continua el termino de luigares

--- COMPRAS2/LUGARES.Designer.cs
+++ COMPRAS2/LUGARES.Designer.cs
@@ -94,6 +94,7 @@ namespace COMPRAS2
             this.btnOK.Size = new System.Drawing.Size(88, 34);
             this.btnOK.TabIndex = 76;
             this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // bTNBack
             // 
--- COMPRAS2/LUGARES.cs
+++ COMPRAS2/LUGARES.cs
@@ -46,9 +46,65 @@ namespace COMPRAS2
             this.dgvLugares.Columns["activo"].Visible = false;
         }
 
+        private async Task<int> Auth()
+        {
+            try
+            {
+                Lugares lugarnew = new Lugares();
+                lugarnew.lugar = txtLugarDeseado.Text;
+
+                string json = JsonConvert.SerializeObject(lugarnew,
+                new JsonSerializerSettings { DefaultValueHandling = DefaultValueHandling.Ignore });
+                var url = HttpMethods.url + "lugares";
+                StatusMessage statusmessage = await HttpMethods.Post(url, json);
+
+
+                if (statusmessage.statuscode == 409)
+                {
+                    MessageBox.Show("error en el servicio, posiblemente el producto ya exista");
+                    return 2;
+                }
+
+                else if (statusmessage.statuscode == 500)
+                {
+                    MessageBox.Show("error en el servicio");
+                    return 2;
+                }
+                else if (statusmessage.statuscode == 201)
+                {
+                    //var auth = JsonConvert.DeserializeObject<Devices>(statusmessage.data);
+                    List<Lugares> devices = JsonConvert.DeserializeObject<List<Lugares>>(statusmessage.data);
+                    MessageBox.Show("PRODUCTO AGREGADO CORRECTAMENTE");
+                    Navigator.backPage(this.Name, this);
+                    return 0;
+                }
+                else if (statusmessage.statuscode == 404)
+                {
+                    MessageBox.Show("error en el servicio, NO encontrado");
+
+                    return 2;
+                }
+                else
+                {
+                    return 2;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Occurrio un error en la respuesta, reintente de nuevo");
+                return 10;
+            }
+        }
+
         private void txtLugarDeseado_Click(object sender, EventArgs e)
         {
             txtLugarDeseado.Clear();
+        }
+
+        private async void btnOK_Click(object sender, EventArgs e)
+        {
+            int status = await Auth();           
         }
     }
 }
